e0bf17e946138d47d9582ad05d20f1a37cf26a82
Angie <angel890817@hotmail.com>
2025-05-30 10:32:56 +1000

    feature: Folder Structure set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
 import App from './App.jsx'
+import Login from './pages/Login.jsx';
+import Expense from './pages/Expense.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/expenses" element={<Expense />} />
+      </Routes>
+    
+    </BrowserRouter>
+    
   </StrictMode>,
 )
